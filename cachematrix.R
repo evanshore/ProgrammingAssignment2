743a536efd3b1a5c1f1d13f83f3878a605ed2db8
Evan Shore <evan.shore@gmail.com>
2014-10-26 12:42:02 -0400

    Assignment done?

--- cachematrix.R
+++ cachematrix.R
@@ -1,18 +1,54 @@
-
+## We want to create and use a cached inverted matrix.
 
 ## makeCacheMatrix: This function creates a special "matrix" object 
 ## that can cache its inverse.
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(matrix = matrix()) {
+     
+     ## Make sure a matrix is entered
+     if(!is.matrix(matrix)) {
+          stop("That's not a matrix. Please enter a matrix")
+     }
+     
+     ## Create object inverted.matrix
+     inverted.matrix <- NULL
 
+     ## Create functions to set/get both the matrix and inverse
+     set.matrix <- function(y) {
+          matrix <<- y
+          inverted.matrix <<- NULL
+     }
+     get.matrix <- function() matrix
+     set.inverse <- function(solve) inverted.matrix <<- solve
+     get.inverse <- function() inverted.matrix
+
+     list(
+          set = set, 
+          get = get, 
+          setmean = setmean,
+          getmean = getmean
+          )
 }
-
 
 ## cacheSolve: This function computes the inverse of the special 
 ## "matrix" returned by makeCacheMatrix above. If the inverse has 
 ## already been calculated (and the matrix has not changed), then 
 ## the cachesolve should retrieve the inverse from the cache.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(user.matrix, ...) {
+
+     ## Define inverted matrix object using get.inverse     
+     inverted.matrix <- user.matrix$get.inverse()
+     
+     ## If it's there, return the inverted matrix
+     if(!is.null(inverted.matrix)) {
+          message("Getting cached inverse matrix")
+          return(inverted.matrix)
+          }
+     
+     ## If it's not there, calculate it
+     inverse.this <- user.matrix$get()
+     inverted.matrix <- solve(inverse.this, ...)
+     user.matrix$set.inverse(inverted.matrix)
+     inverted.matrix
 }
